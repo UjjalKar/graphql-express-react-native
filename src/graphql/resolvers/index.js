e59b0dc8e50b6c35e88c493d53f23b84c469467b
Ujjal Kar <ujjalkar17@gmail.com>
2019-04-09 01:57:41 +0530

    done part 2

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,7 +1,15 @@
+import GraphqlDate from "graphql-date";
 import TweetResolvers from "./tweet.resolvers";
 
 export default {
+  Date: GraphqlDate,
   Query: {
+    getTweet: TweetResolvers.getTweet,
     getTweets: TweetResolvers.getTweets
+  },
+  Mutation: {
+    createTweet: TweetResolvers.createTweet,
+    updateTweet: TweetResolvers.updateTweet,
+    deleteTweet: TweetResolvers.deleteTweet
   }
 };
--- src/models/Tweet.js
+++ src/models/Tweet.js
@@ -1,7 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
-const TweetSchema = new Schema({
-  text: String
-});
+const TweetSchema = new Schema(
+  {
+    text: String
+  },
+  {
+    timestamps: true
+  }
+);
 
 export default mongoose.model("Tweet", TweetSchema);
